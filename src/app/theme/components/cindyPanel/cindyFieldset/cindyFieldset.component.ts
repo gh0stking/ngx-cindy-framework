42c3f789423a86d29ab3787861bed41887a14c1c
gh0stking <dev.gh0stking@gmail.com>
2017-09-18 17:55:22 +0800

    fieldset

--- src/app/theme/components/cindyPanel/cindyFieldset/cindyFieldset.component.ts
+++ src/app/theme/components/cindyPanel/cindyFieldset/cindyFieldset.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'c-fieldset',
-    templateUrl: './cindyFieldset.component.html'
+    template: `
+        <p-fieldset 
+            [legend]="legend" 
+            [toggleable]="toggleable"
+            [collapsed]="collapsed"
+            [style]="style"
+            [styleClass]="styleClass"
+            (onBeforeToggle)="onBeforeToggleEvent()"
+            (onAfterToggle)="onAfterToggleEvent()">
+            <ng-content></ng-content>
+        </p-fieldset>
+    `
 })
 export class CindyFieldset {
+    @Input() legend: string;
+    @Input() toggleable: boolean = false;
+    @Input() collapsed: boolean = false;
+    @Input() style: string;
+    @Input() styleClass: string;
 
+    @Output() onBeforeToggle: EventEmitter<any> = new EventEmitter();
+    @Output() onAfterToggle: EventEmitter<any> = new EventEmitter();
+
+    onBeforeToggleEvent() {
+        this.onBeforeToggle.emit();
+    }
+
+    onAfterToggleEvent() {
+        this.onAfterToggle.emit();
+    }
 }
