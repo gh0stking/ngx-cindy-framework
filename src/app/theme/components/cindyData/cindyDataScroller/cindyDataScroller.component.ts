19fb375cd8bf01bd3b5c56964934c37944202bee
gh0stking <dev.gh0stking@gmail.com>
2017-09-26 17:25:44 +0800

    datascroller

--- src/app/theme/components/cindyData/cindyDataScroller/cindyDataScroller.component.ts
+++ src/app/theme/components/cindyData/cindyDataScroller/cindyDataScroller.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ContentChild, ViewChild } from '@angular/core';
+import { CindyTemplate } from '../../../directives/common/cindyTemplate.directive';
+import { DataScroller } from 'primeng/primeng';
 
 @Component({
     selector: 'c-dataScroller',
-    templateUrl: './cindyDataScroller.component.html'
+    template: `
+        <p-dataScroller #ds
+            [value]="value"
+            [rows]="rows"
+            [inline]="inline"
+            [scrollHeight]="scrollHeight"
+            [loader]="loader"
+            [style]="style"
+            [styleClass]="styleClass"
+            (onLazyLoad)="onLazyLoadEvent($event)">
+            <ng-content select="c-header"></ng-content>
+            <ng-content select="c-footer"></ng-content>
+            <ng-content [ngTemplateOutlet]='template'></ng-content>
+        </p-dataScroller>
+    `
 })
 export class CindyDataScroller {
+    @ContentChild(CindyTemplate) template: CindyTemplate;
+    @ViewChild("ds") ds: DataScroller;
 
+    @Input() value: any[];
+    @Input() rows: number;
+    @Input() inline: boolean;
+    @Input() scrollHeight: any;
+    @Input() loader: any;
+    @Input() style: string;
+    @Input() styleClass: string;
+
+    @Output() onLazyLoad: EventEmitter<any> = new EventEmitter();
+
+    onLazyLoadEvent(event) {
+        this.onLazyLoad.emit(event);
+    }
+
+    public reset() {
+        this.ds.reset();
+    }
 }
