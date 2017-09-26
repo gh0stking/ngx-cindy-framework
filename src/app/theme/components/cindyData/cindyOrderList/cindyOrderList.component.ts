d547071f62a21ce612aba65a2464a1d8b46e346b
gh0stking <dev.gh0stking@gmail.com>
2017-09-26 17:35:58 +0800

    orderlist

--- src/app/theme/components/cindyData/cindyOrderList/cindyOrderList.component.ts
+++ src/app/theme/components/cindyData/cindyOrderList/cindyOrderList.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ContentChild, ViewChild } from '@angular/core';
+import { CindyTemplate } from '../../../directives/common/cindyTemplate.directive';
 
 @Component({
     selector: 'c-orderList',
-    templateUrl: './cindyOrderList.component.html'
+    template: `
+        <p-orderList 
+            [value]="value"
+            [header]="header"
+            [style]="style"
+            [styleClass]="styleClass"
+            [listStyle]="listStyle"
+            [responsive]="responsive"
+            [filterBy]="filterBy"
+            [metaKeySelection]="metaKeySelection"
+            [dragdrop]="dragdrop"
+            [dragdropScope]="dragdropScope"
+            [filterPlaceHolder]="filterPlaceHolder"
+            (onReorder)="onReorderEvent($event)"
+            (onSelectionChange)="onSelectionChangeEvent($event)"
+            (onFilterEvent)="onFilterEventEvent($event)">
+            <ng-content [ngTemplateOutlet]='template'></ng-content>
+        </p-orderList>
+    `
 })
 export class CindyOrderList {
+    @Input() value: any[];
+    @Input() header: string;
+    @Input() style: string;
+    @Input() styleClass: string;
+    @Input() listStyle: string;
+    @Input() responsive: boolean;
+    @Input() filterBy: string;
+    @Input() metaKeySelection: boolean = true;
+    @Input() dragdrop: boolean;
+    @Input() dragdropScope: string;
+    @Input() filterPlaceHolder: string;
 
+    @Output() onReorder: EventEmitter<any> = new EventEmitter();
+    @Output() onSelectionChange: EventEmitter<any> = new EventEmitter();
+    @Output() onFilterEvent: EventEmitter<any> = new EventEmitter();
+
+    onReorderEvent(event) {
+        this.onReorder.emit(event);
+    }
+
+    onSelectionChangeEvent(event) {
+        this.onSelectionChange.emit(event);
+    }
+
+    onFilterEventEvent(event) {
+        this.onFilterEvent.emit(event);
+    }
 }
