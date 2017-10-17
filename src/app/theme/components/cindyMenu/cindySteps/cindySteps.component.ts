dcad349b0dbba3eda3ed9d694b049cc56f87c385
gh0stking <dev.gh0stking@gmail.com>
2017-10-17 20:09:40 +0800

    steps

--- src/app/theme/components/cindyMenu/cindySteps/cindySteps.component.ts
+++ src/app/theme/components/cindyMenu/cindySteps/cindySteps.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'c-cindySteps',
-    templateUrl: './cindySteps.component.html'
+    template: `
+        <p-steps 
+            [model]="model"
+            [activeIndex]="activeIndex"
+            [readonly]="readonly"
+            [style]="style"
+            [styleClass]="styleClass"
+            (activeIndexChange)="activeIndexChangeEvent($event)">
+        </p-steps>
+    `
 })
 export class CindySteps {
+    @Input() model: any;
+    @Input() activeIndex: number = 0;
+    @Input() readonly: boolean = true;
+    @Input() style: string;
+    @Input() styleClass: string;
 
+    @Output() activeIndexChange: EventEmitter<any> = new EventEmitter();
+
+    activeIndexChangeEvent(event) {
+        this.activeIndexChange.emit(event);
+    }
 }
